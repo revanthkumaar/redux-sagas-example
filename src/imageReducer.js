81b30d383f8aaa8868fb83729915ede41a379ed6
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-21 11:40:36 +0530

    as
    dasdasd

--- src/imageReducer.js
+++ src/imageReducer.js
@@ -4,39 +4,33 @@ const initialState = {
     loading:false,
     error:false
 }
-
-
 //REDUCER 
-
 const reducer = (state=initialState,action) => {
     switch(action.type){
         case 'FETCHED_IMAGE':
             return{
 
             };
-
         case 'REQUESTED_IMAGE':
             return{
                 url:'',
                 loading:true,
                 error:false
             };
-        case 'REQUESTED_IMAGE_SUCCESS'{
+        case 'REQUESTED_IMAGE_SUCCESS':
             return {
                 url: action.url,
                 loading: false,
                 error: false
-            }
-        };
-        case 'REQUESTED_IMAGE_ERROR'{
+            };
+        
+        case 'REQUESTED_IMAGE_ERROR':
             return {
                 url: '',
                 loading: false,
                 error: true
-            }
-        };
-
-        default:
+            };
+            default:
             return state;
     }
 
